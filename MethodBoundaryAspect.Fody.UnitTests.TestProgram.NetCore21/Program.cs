064a91130806b6d3f85cc9bec937c7f326187db8
Martin Ayasse <m.ayasse@vescon.com>
2018-12-04 16:24:15 +0100

    #53 added netcore21 testprogram

---
+++ MethodBoundaryAspect.Fody.UnitTests.TestProgram.NetCore21/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace MethodBoundaryAspect.Fody.UnitTests.TestProgram.NetCore21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //If no debugger is attached launch the debugger
+            if (Debugger.IsAttached == false)
+                Debugger.Launch();
+            Debugger.Break();
+
+            var testClass = new TestClass();
+            testClass.DoIt(7);
+        }
+    }
+}
